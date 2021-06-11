08711e330f2765ea3a95499266cd83e7685ca393
3kanAlpha <39447635+3kanAlpha@users.noreply.github.com>
2021-06-11 11:01:32 +0900

    とりあえずアニメーションつけた

--- assets/main.js
+++ assets/main.js
@@ -14,7 +14,22 @@ function playVideo() {
 }
 
 function onClick(event) {
-    console.log("you clicked (" + event.pageX + ", " + event.pageY + ")");
+    // console.log("you clicked (" + event.pageX + ", " + event.pageY + ")");
+
+    const x = event.pageX;
+    const y = event.pageY;
+
+    let fav = document.createElement("div");
+    fav.innerHTML = "<i class='large material-icons'>favorite</i>";
+    fav.style.left = x + "px";
+    fav.style.top = y + "px";
+    document.body.appendChild(fav);
+
+    fav.className = "fav";
+
+    fav.addEventListener("animationend", function() {
+        this.parentNode.removeChild(this);
+    }, false);
 }
 
 $(function() {
